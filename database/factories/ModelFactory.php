dac121e9e17fc8c156ed4366d5eee55c8ebdbd94
李佳纯 <2741354172@qq.com>
2018-04-19 15:21:36 +0800

    2018 04 19 初步掌握精髓所在

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,14 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+//填充数据表posts
+$factory->define(App\Post::class, function(Faker\Generator $faker){
+    return [
+        'title' => $faker->sentence(6, true),
+        'content' => $faker->text(500),
+        'user_id' => function() {
+            return factory(\App\User::class)->create()->id;
+        }
+    ];
+});
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', 'Index\IndexController@getIndex');//初尝试 win
+
+//文章列表页
+Route::get('/posts','\App\Http\Controllers\PostController@index');//首页
+
+
+//创建文章
+Route::get('/posts/create','\App\Http\Controllers\PostController@create');
+Route::post('/posts','\App\Http\Controllers\PostController@store');
+//编辑文章
+Route::get('/posts/{post}/edit','\App\Http\Controllers\PostController@edit');
+Route::put('/posts/{post}','\App\Http\Controllers\PostController@update');
+//删除文章
+Route::get('/posts/delete','\App\Http\Controllers\PostController@delete');
+
+//文章详情页
+Route::get('/posts/{post}','\App\Http\Controllers\PostController@show');
